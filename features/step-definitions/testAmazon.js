40c0f7aa4e517add0c896c37096e468faed688c5
SallyZhang <zy_sally@163.com>
2015-02-03 14:45:13 +0800

    Try change

--- features/step-definitions/testAmazon.js
+++ features/step-definitions/testAmazon.js
@@ -6,20 +6,17 @@ chai.use(chaiAsPromised);
 var testAmazonWrapper = function(){
 
 	this.Given(/^I open Amazon website$/, function(next) {
-		browser.driver.navigate().to('http://www.amazon.com/');
-		browser.driver.manage().window().maximize();
 
 		next();
 	});
 
 	this.When(/^I search book "([^"]*)"$/, function(book, next) {
-		browser.driver.findElement(By.id('twotabsearchtextbox')).sendKeys(book);
-		browser.driver.findElement(By.className('nav-submit-input')).click();
+
         next();
     });
 
     this.Then(/^I should see the book "([^"]*)" in search result$/, function(book,next) {
-    	chai.expect(browser.driver.findElement(By.linkText(book))).to.exist;
+    	
         next();
     });
 	
